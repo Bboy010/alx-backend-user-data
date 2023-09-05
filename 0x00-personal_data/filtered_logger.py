f8c1a158b99d4fe273a7afc68b28c3cdc9ba7cad
Bboy010 <hkoffianderson@gmail.com>
2023-09-04 17:25:45 -0700

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,5 +5,6 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """Define filter datum"""
     regex = '|'.join(f'({field}=.*?)({separator}|$)' for field in fields)
     return re.sub(regex, rf'\1{redaction}\2', message)
