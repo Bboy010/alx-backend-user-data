90420a1f89a8f0e09f2c8f9c33fc4f72a177d89c
Bboy010 <hkoffianderson@gmail.com>
2023-09-05 09:06:18 -0700

    Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -61,7 +61,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.environ.get('PERSONAL_DATA_DB_NAME', 'my_db')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
     connector = mysql.connector.connect(
         user=username,
         password=password,
@@ -69,3 +69,52 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         database=db_name
     )
     return connector
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str] = None):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields or []
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in incoming log records"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def main():
+    """ Display each db row under a filtered format """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        message = (
+            "name={}; email={}; phone={}; ssn={};password={};"
+            "ip={};last_login={};user_agent={};"
+        ).format(
+            row[0], row[1], row[2], row[3],
+            row[4], row[5], row[6], row[7]
+        )
+        log_record = logging.LogRecord(
+            "user_data",
+            logging.INFO,
+            None,
+            None,
+            message,
+            None,
+            None
+        )
+        formatter = RedactingFormatter(fields=PII_FIELDS)
+        print(formatter.format(log_record))
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
