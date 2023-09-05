3c6fb1b49568d6707e4179cc8239bea55f219833
Bboy010 <hkoffianderson@gmail.com>
2023-09-05 09:12:35 -0700

    encrypt

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Task 5.Let Encrypting passwords"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Returns a salted, hashed password,
+       which is a byte string
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Returns a boolean value indicating
+       whether or not the provided password
+       matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
